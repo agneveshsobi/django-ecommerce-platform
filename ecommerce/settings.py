4afac9a97a500a21affad70ac0fb8e3bb4be3db8
Agnevesh <agneveshsobi@gmail.com>
2026-03-07 02:42:37 +0530

    fixed static files storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,7 +80,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
