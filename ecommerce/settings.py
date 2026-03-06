262480a40621818b204f3e6d948f2a660a5c800b
Agnevesh <agneveshsobi@gmail.com>
2026-03-06 16:24:26 +0530

    fixed database config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,7 +3,6 @@ Django settings for ecommerce project.
 """
 
 import os
-import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -59,10 +58,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
